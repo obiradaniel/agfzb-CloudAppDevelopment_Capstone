31ce17774bb81d6941c1cbdd8fd8449bb9bb8366
obiradaniel <obiradaniel@gmail.com>
2022-05-10 02:07:18 +0300

    Finalized models for CarMake and CarModel with admin interfacing

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarModel, CarMake
 
 
 # Register your models here.
+#admin.site.register(CarMake)
+#admin.site.register(CarModel)
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'dealer_id', 'car_type', 'year']
+    list_filter = ['car_type', 'year']
+    search_fields = ['name', 'dealer_id', 'car_type', 'year']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description', 'manufacturer' ]
+    list_filter = ['name', 'description', 'manufacturer' ]
+    search_fields = ['name', 'description', 'manufacturer' ]
 
 # Register models here
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,14 +10,34 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 class CarMake(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    #id = models.BigAutoField(primary_key=True)
     name = models.CharField(null=False, max_length=30)
     description = models.CharField(max_length=1000)
-    
-    def __str__(self):
-        return self.Name + " " + self.Description
-    
+    Toyota = "Toyota"
+    Volkswagen = "Volkswagen"
+    Mitsubishi = "Mitsubishi"
+    Ford = "Ford"
+    Nissan = "Nissan"
+    Honda = "Honda"
+    Hyundai = "Hyundai"
+    Suzuki = "Suzuki"
+    Mazda = "Mazda"
+    Subaru = "Subaru"
+    manufacturer_choices = [
+    (Toyota,"Toyota"),
+    (Volkswagen, "Volkswagen"),
+    (Mitsubishi, "Mitsubishi"),
+    (Ford, "Ford"),
+    (Nissan, "Nissan"),
+    (Honda, "Honda"),
+    (Hyundai, "Hyundai"),
+    (Suzuki, "Suzuki"),
+    (Mazda, "Mazda"),
+    (Subaru, "Subaru")]
+    manufacturer = models.CharField(max_length=20, choices=manufacturer_choices, default=Toyota)
 
+    def __str__(self):
+        return self.manufacturer + " "+ self.name + " " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -30,16 +50,83 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(null=False, max_length=30)
-    Description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    dealer_id = models.IntegerField(null=False)
+    Sedan = "Sedan" 
+    SUV = "Volkswagen" 
+    Truck = "Mitsubishi" 
+    MiniVan  = "Mitsubishi" 
+    Van = "Mitsubishi" 
+    Bus = "Mitsubishi" 
+    MiniBus = "Mitsubishi" 
+    Bus = "Mitsubishi" 
+    Wagon = "Mitsubishi" 
+    Pick_up = "Pick Up"
+    cartype_choices = [
+    (Sedan,"Sedan"),
+    (SUV, "Volkswagen"),
+    (Truck, "Mitsubishi"),
+    (MiniVan, "Mitsubishi"),
+    (Van, "Mitsubishi"),
+    (Bus, "Mitsubishi"),
+    (MiniBus, "Mitsubishi"),
+    (Bus, "Mitsubishi"),
+    (Wagon, "Mitsubishi"),
+    (Pick_up, "Pick Up")]
+    car_type = models.CharField(null=False, max_length=20, choices=cartype_choices, default=Sedan)
+    year = models.DateField(null=False)
+    # steering
+    # transmission
+    # four_wheel
+    # fuel
+    # price
+    # condition
+    # engine_model
+    # engine_capacity_
+    # doors
+    # passengers
+    # color_exterior
+    # weight
+
     
     def __str__(self):
-        return self.Name + " " + self.Description
+        return self.name + " " + self.description + " " + str(self.year)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
+class CarDealer:
+    def __init__(self, id, city, state, st, address, zip, lat, long, short_name, full_name):
+        self.id = id,
+        self.city = city, 
+        self.state = state, 
+        self.st = st, 
+        self.address = address, 
+        self.zip = zip, 
+        self.lat = lat,
+        self.long = long,
+        self.short_name = short_name,
+        self.full_name = full_name
 
+    def __str__(self):
+        return self.short_name + " " + self.city + " " + self.st
+
+false = False
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, another):
+        self.id=id
+        self.name=name
+        self.dealership=dealership
+        self.review=review
+        self.purchase=purchase
+        self.purchase_date=purchase_date
+        self.car_make=car_make
+        self.car_model=car_model
+        self.car_year=car_year
+        self.another=another
 
+    def __str__(self):
+        return self.review + " " + self.car_model + "by " + self.name
 
 
 django_model_fields_rels_etc = ['Aggregate',
